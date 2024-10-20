812005ead5bb060d1081909ff72cc1a653b64138
bulutcnsu <cnsubulutt@gmail.com>
2024-10-21 00:55:38 +0300

    updated

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# CLEANBLOG" 
+"# POSTING BLOGS"
--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
+const path = require('path');
 
-app.get('/', function (req, res) {
-  const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
-  res.send(blog);
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public/temp'));
+
+/*app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname, 'public/temp/index.html'));
 });
 
-app.listen(3000);
+*/
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+
+app.get("/about", (req, res) => {
+  res.render('about');
+});
+
+app.get("/add_post", (req, res) => {
+  res.render('add_post');
+});
+
+let port = 3000;
 
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda başlatıldı..`);
-  });
+  console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
